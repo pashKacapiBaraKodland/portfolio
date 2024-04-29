5d93319bd08dd58fe2da4c03642045d63469738f
pashKa <156570934+pashKacapiBaraKodland@users.noreply.github.com>
2024-04-29 19:12:28 +1100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,42 @@
+#Импорт
+from flask import Flask, render_template,request, redirect
+
+
+
+app = Flask(__name__)
+
+#Запуск страницы с контентом
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/rs', methods=['POST'])
+def form_res():
+    email = request.form.get('email')
+    text = request.form.get('text')
+    
+    with open('form.txt', 'a',) as f:
+        f.write(email+' '+text+'\n')
+    return render_template('form_result.html',
+                           email=email,
+                           text=text
+                           )
+#Динамичные скиллы
+@app.route('/', methods=['POST'])
+def process_form():
+    button_python = request.form.get('button_python')
+    button_discord = request.form.get('button_discord')
+    button_html = request.form.get('button_html')
+    button_db = request.form.get('button_db')
+    return render_template('index.html', button_python=button_python,
+                            button_discord=button_discord,
+                            button_html=button_html,
+                            button_db=button_db
+
+                           
+                           
+                           )
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
